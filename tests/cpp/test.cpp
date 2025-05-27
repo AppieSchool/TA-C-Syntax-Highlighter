59708e48d23f10c0eaa3c0bc8513f071c3839f8e
Kaede <sukhovii.daniil@gmail.com>
2025-05-27 20:55:17 +0200

    tests

--- tests/cpp/test.cpp
+++ tests/cpp/test.cpp
@@ -1,3 +1,9 @@
 //
 // Created by Kaede on 2025-05-27.
 //
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
